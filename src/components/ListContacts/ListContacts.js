8460184389cbd42b403e2b3259cb2b3797716c10
vrrajkum <vivek.rajkumar@protonmail.com>
2018-04-30 21:21:17 -0400

    Add filtering functionality.

--- src/components/ListContacts/ListContacts.js
+++ src/components/ListContacts/ListContacts.js
@@ -7,20 +7,48 @@ export class ListContacts extends React.Component {
   }
 
   // Update state.query with search input
-  handleChange = e => {
+  updateQuery = e => {
     this.setState({
       query: e.target.value
     });
   }
 
+  // Reset state.query
+  clearQuery = () => {
+    this.setState({
+      query: ''
+    });
+  }
+
   render() {
+    const { query } = this.state;
+    const { contacts, onRemove } = this.props;
+
+    // Filter displayed contacts based on if contacts' names contain query
+    let filteredContacts = query === ''
+    ? contacts
+    : contacts.filter(contact => contact.name.toLowerCase().includes(query.toLowerCase()));
+
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
-          <input className="search-contacts" placeholder="Search contacts" value={this.state.query} onChange={this.handleChange} />
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={query}
+            onChange={this.updateQuery} />
         </div>
+
+        {filteredContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {filteredContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className="contact-list">
-          {this.props.contacts.map(contact =>
+          {filteredContacts.map(contact =>
             <li key={contact.id} className="contact-list-item">
               <div className="contact-avatar" style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -29,7 +57,7 @@ export class ListContacts extends React.Component {
                 <p>{contact.name}</p>
                 <p><a href={`https://twitter.com/${contact.handle}`}>{contact.handle}</a></p>
               </div>
-              <button className="contact-remove" onClick={() => this.props.onRemove(contact)}>
+              <button className="contact-remove" onClick={() => onRemove(contact)}>
                 Remove
               </button>
             </li>)}
@@ -40,5 +68,6 @@ export class ListContacts extends React.Component {
 }
 
 ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired
+  contacts: PropTypes.array.isRequired,
+  onRemove: PropTypes.func.isRequired
 };

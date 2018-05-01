e60540fb5032a61b125b91149e920948570b74ac
vrrajkum <vivek.rajkumar@protonmail.com>
2018-05-01 17:26:44 -0400

    Add AJAX requests.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { ListContacts } from './components/ListContacts/ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends React.Component {
   state = {
-    contacts: [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "@karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "@richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "@tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-]
+    contacts: []
+  }
+
+  async componentDidMount() {
+    let contacts = await ContactsAPI.getAll();
+    this.setState({
+      contacts: contacts
+    });
   }
 
   // Filter a contact out of state.contacts
@@ -30,6 +19,7 @@ class App extends React.Component {
     this.setState({
       contacts: this.state.contacts.filter(c => c.id !== contact.id)
     });
+    ContactsAPI.remove(contact);
   }
 
   render() {
